0caead794d29ad53358efae593b8ba074145e5e3
pablo.henrique <pablo.henrique@ati.pe.gov.br>
2025-04-16 13:40:25 -0300

    criando um retorno para as rotas products e sessions

--- fullstack-auth-template-main/fullstack-auth-template-main/src/controllers/products-controller.ts
+++ fullstack-auth-template-main/fullstack-auth-template-main/src/controllers/products-controller.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express"
 
 class ProductsController {
-  async index(request: Request, response: Response) {}
+  async index(request: Request, response: Response) {
 
-  async create(request: Request, response: Response) {}
+    response.json({ message: "Lista de produtos"})
+  }
+
+  async create(request: Request, response: Response) {
+
+    response.json({ message: "O produto foi criado"})
+  }
 }
 
 export { ProductsController }
