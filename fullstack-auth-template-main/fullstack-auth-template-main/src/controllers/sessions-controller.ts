491d9cbc039e8dce50cdd2197049e41cb65ef657
pablo <devpabloh@gmail.com>
2025-04-22 22:21:28 -0300

    atualização testando do jwt

--- fullstack-auth-template-main/fullstack-auth-template-main/src/controllers/sessions-controller.ts
+++ fullstack-auth-template-main/fullstack-auth-template-main/src/controllers/sessions-controller.ts
@@ -1,8 +1,22 @@
+import { AppError } from "@/utils/AppError"
 import { Request, Response } from "express"
 
 class SessionsController {
   async create(request: Request, response: Response) {
-    return response.json({ message: process.env.USER_ID})
+
+  const fakeUser = {
+    id: 1,
+    name: "PabloHenrique",
+    password: 123456,
+  }
+
+  const {name, password} = request.body
+
+  if(fakeUser.name !== name || fakeUser.password !== password){
+    throw new AppError("Nome e/ou senha incorreta", 401)
+  }
+
+    return response.json({ message: "ok"})
   }
 }
 
